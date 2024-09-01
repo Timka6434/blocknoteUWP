2dcdb95a7227ad55d7f20b716a4242032a2b4d5f
Timka6434 <timka6434@mail.ru>
2024-09-01 19:24:56 +0500

    Добавьте файлы проекта.

---
+++ Models/NoteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blocknoteUWP.Models
+{
+    internal class NoteModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DataCreate { get; set; } = DateTime.Now;
+    }
+}
